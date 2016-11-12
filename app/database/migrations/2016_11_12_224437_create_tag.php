a41ae5daef21564e89026ebe5e1a10de490e9cc9
Henry - Laptop <hps1945@live.co.uk>
2016-11-12 23:34:47 +0000

    Tidied up migration file names and class names

--- app/database/migrations/2016_11_12_224437_create_tag.php
+++ app/database/migrations/2016_11_12_224437_create_tag.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ActivityTag extends Migration {
+class CreateTagTable extends Migration {
 
 	/**
 	 * Run the migrations.
