908b1a84699b9f33a804e7d5e1360f8ef8a267cd
George Broadley <gbroadley96@gmail.com>
2016-11-12 16:55:10 +0000

    Created users table

---
+++ app/database/migrations/2016_11_12_151410_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->increments('id')->unsigned();
+			$table->string('name');
+			// $table->integer('university_id')->unsigned();
+			$table->string('email');
+			$table->string('password');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->drop();
+		});
+	}
+
+}
