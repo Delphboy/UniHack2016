fd019425e139e40f30a7dbf06f517a933f56721a
George Broadley <gbroadley96@gmail.com>
2016-11-12 18:41:44 +0000

    Created Auth

--- app/database/migrations/2016_11_12_151410_create_users_table.php
+++ app/database/migrations/2016_11_12_151410_create_users_table.php
@@ -5,34 +5,34 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('users', function(Blueprint $table)
-		{
-			$table->increments('id')->unsigned();
-			$table->string('name');
-			// $table->integer('university_id')->unsigned();
-			$table->string('email');
-			$table->string('password');
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            // $table->integer('university_id')->unsigned();
+            $table->string('email');
+            $table->string('password');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('users', function(Blueprint $table)
-		{
-			$table->drop();
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->drop();
+        });
+    }
 
 }
