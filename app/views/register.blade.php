9bcbeaab80a8a0c4d5c08b113243bf1a2691f6ea
Henry - Laptop <hps1945@live.co.uk>
2016-11-12 18:32:59 +0000

    Login and register fix
    Added login view and fixed the register view

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -1,33 +1,23 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-    <title>Register</title>
-</head>
+@section('content')
+    <div class="col-md-5">
+        {{ Form::open( array('url' => '/register', 'method' => 'POST')) }}
+        <div class="form-group">
+            {{Form::text('first_name', null, array('placeholder' => 'First Name'))}}
+        </div>
+        <div class="form-group">
+            {{Form::text('last_name', null, array('placeholder' => 'Last Name'))}}
+        </div>
+        <div class="form-group">
+            {{Form::text('register_email', null, array('placeholder' => 'Email'))}}
+        </div>
+        <div class="form-group">
+            {{Form::password('password', array('placeholder' => 'Password'))}}
+        </div>
+        <div class="form-group">
+            {{Form::password('password_confirmation', array('placeholder' => 'Confirm Password'))}}
+        </div>
 
-<body>
-<form id="register" action="POST">
-    First Name: <input type="text" name="first-name" placeholder="First Name" />
-    <br />
-    Surname: <input type="text" name="surname" placeholder="Surname" />
-    <br />
-
-    What University do you attend?
-    <input type="text" name="university" placeholder="University" />
-    <br />
-
-    What society are you joining?
-    <input type="text" name="society" placeholder="Society" />
-    <br />
-
-    What is your year of study?
-    <br />
-    <input type="radio" name="study-year" value="1" checked> 1
-    <br>
-    <input type="radio" name="study-year" value="2"> 2
-    <br>
-    <input type="radio" name="gendestudy-year" value="3"> 3
-    <br />
-</form>
-</body>
-
-</HTML>
+        {{Form::submit('Sign Up',array('class'=> 'btn btn-primary'))}}
+        {{ Form::close(); }}
+    </div>
+@stop
